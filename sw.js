d9f723af31a7607ad3235685fc285e99f760ac7e
lichen1972 <lichen.ng@gmail.com>
2026-09-23 15:35:05 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'timer-bars-v16';
+const CACHE = 'timer-bars-v17';
 const FILES = ['./', 'index.html', 'manifest.json', 'icons/icon-192.png', 'icons/icon-512.png', 'icons/icon-maskable-192.png', 'icons/icon-maskable-512.png', 'icons/apple-touch-icon.png', 'icons/logo.png'];
 
 self.addEventListener('install', e => {
